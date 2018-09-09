c8b1c4f404eb594662fcf799a766d7de04a13bd3
Zapel <zapel176@ukr.net>
2018-09-09 20:06:32 +0300

    POstep5

--- src/test/java/PageObjects/Application.java
+++ src/test/java/PageObjects/Application.java
@@ -3,24 +3,25 @@ package PageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import javax.swing.*;
 import java.util.Set;
 
-import static java.util.stream.Collectors.toSet;
-
 public class Application {
     private WebDriverWait wait;
     private WebDriver driver;
     private RegistrationPage registrationPage;
+    private AdminPanelLoginPage adminPanelLoginPage;
+    private CustomerListPage customerListPage;
 
     public Application() {
         driver = new ChromeDriver();
         wait = new WebDriverWait(driver, 10);
 
         registrationPage = new RegistrationPage(driver);
+        adminPanelLoginPage = new AdminPanelLoginPage(driver);
+        customerListPage = new CustomerListPage(driver);
     }
 
     public void quit() {
@@ -36,10 +37,8 @@ public class Application {
         registrationPage.cityInput().sendKeys(customer.getCity());
 
         registrationPage.selectCountry(customer.getCountry());
-
         //new Select (driver.findElement(By.name("country_code"))).selectByValue(customer.getCountry());
         //new Select(driver.findElement(By.name("zone_code"))).selectByValue(customer.getZone());
-
         registrationPage.emailInput().sendKeys(customer.getEmail());
         registrationPage.phoneInput().sendKeys(customer.getPhone());
         registrationPage.passwordInput().sendKeys(customer.getPassword());
@@ -52,17 +51,9 @@ public class Application {
     }
 
     public Set<String> getCustomerIds() {
-        driver.get("http://localhost/litecart/admin");
-        if (driver.findElements(By.id("box-login")).size() > 0) {
-            driver.findElement(By.name("username")).sendKeys("admin");
-            driver.findElement(By.name("password")).sendKeys("admin");
-            driver.findElement(By.name("login")).click();
-            wait.until((WebDriver d) -> d.findElement(By.id("box-apps-menu")));
-
+        if(adminPanelLoginPage.open().isOnThisPage()) {
+            adminPanelLoginPage.enterUsername("admin").enterPassword("admin").submitLogin();
         }
-        driver.get("http://localhost/litecart/admin/?app=customers&doc=customers");
-        return driver.findElements(By.cssSelector("table.dataTable tr.row")).stream()
-                .map(e -> e.findElements(By.tagName("td")).get(2).getText())
-                .collect(toSet());
+        return customerListPage.open().getCustomerIds();
     }
 }
